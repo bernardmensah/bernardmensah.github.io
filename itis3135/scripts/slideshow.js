3855175af8ce1295a8379b70ce70136d81aa67f6
Bernard Mensah <bernardwesley3@gmail.com>
2021-10-23 20:17:13 -0400

    slideshow completion

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,6 +1,7 @@
 
-$(function()
+$(document).ready(function()
 {
+    //array of images
     var pictures = [
         ["images/1.jpg"],
         ["images/3.jpg"],
@@ -11,31 +12,60 @@ $(function()
     ];
 
     var picturesCount = pictures.length;
-    var cur = 1;
 
     var $images = new Array();
+
+    //this for-loop loops through the images adds the image attribute and pushes it into the $images array
     for(var i = 0; i < picturesCount-1; i++)
     {
-        $images.push($('<img>').attr('src', pictures[i][0]).css({ width: 300, height: 300 }))
+        $images.push($('<img>').attr('src', pictures[i][0]).css({ width: 500, height: 500 }))
     }
 
+    $("#picture-holder").css({width: 500, height: 500 });
+    $("#picture-holder").append("<ul>");
+    $("#picture-holder ul").css({width: (picturesCount) * 500, marginLeft: - 500});
 
-    $("#pics-holder").append("<ul>");
-    $("#pics-holder ul").css("width", (picturesCount) * 990);
 
+    //This for-loop appends the list of images except the last image in the array to the picture holder ul div
     for(var i = 0; i < picturesCount-1; i++)
     {
         var $li = $("<li>").append($images[i]);
-        $("#pics-holder ul").append($li);
+        $("#picture-holder ul").append($li);
     }
 
-    var $last_li = $("<li><img style = width:300px src= '" + pictures[pictures.length-1][0] + "'/></li>");
-    $("#pics-holder ul").prepend($last_li);
+    //the last image in the array is put in front of the others
+    var $last_li = $("<li>").append($('<img>').attr('src', pictures[pictures.length-1][0]).css({ width: 500, height: 500 }));
+    $("#picture-holder ul").prepend($last_li);
 
-    var $right = $("<div class = 'right-handle'>");
-    $("#pics-holder").append($right);
-    $("right-handle").click(function()
-    {
 
-    })
-})
+    //this function allows you to get the previous picture in your slideshow
+    function getPreviousPicture() {
+        $('#picture-holder ul').animate({
+            left: + 500
+        }, 200, function () {
+            $('#picture-holder ul li:last-child').prependTo('#picture-holder ul');
+            $('#picture-holder ul').css('left', '');
+        });
+    };
+
+    //this function allows you to get the next picture in your slideshow
+    function getNextPicture() {
+        $('#picture-holder ul').animate({
+            left: - 500
+        }, 200, function () {
+            $('#picture-holder ul li:first-child').appendTo('#picture-holder ul');
+            $('#picture-holder ul').css('left', '');
+        });
+    };
+
+
+    //function call for when the previous button is click
+    $('a.previous').click(function () {
+        getPreviousPicture();
+    });
+
+    //function call for when the next button is click
+    $('a.next').click(function () {
+        getNextPicture();
+    });
+});
