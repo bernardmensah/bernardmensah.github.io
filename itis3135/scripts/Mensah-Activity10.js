826aafb636b59d303e283cd53532067d610fec47
Bernard Mensah <bernardwesley3@gmail.com>
2021-11-07 13:23:40 -0500

    activity 10

--- itis3135/scripts/Mensah-Activity10.js
+++ itis3135/scripts/Mensah-Activity10.js
@@ -1,15 +1,41 @@
 $(document).ready(function() {
 
+    // preload the image for each link
+    $(".image-link").each(function()
+    {
+            var swappedImage = new Image();
+            swappedImage.src = $(this).attr("href");
+    });
 
-        // preload the image for each link
 
-        // set up the event handlers for each link
+    // set up the event handlers for each link
+    $(".image-link").click(function(evt)
+    {
+        // start slide show
+        var imageURL = $(this).attr("href");
+        
+        var caption = $(this).attr("title");
+        console.log($(this).attr("title"));
+        $("#caption").fadeOut(1000);
+        $("#image").fadeOut(1000,
+                function() {
+                        // get the image URL and caption for each image and animate the caption
+                        $("#image").attr("src", imageURL).fadeIn(3000);
+                        $("#caption").text(caption);
+                        $("#caption").animate({fontSize: "2em"}).fadeIn(3000);   
+                }
+            ); 
 
-    			// get the image URL and caption for each image and animate the caption
+        
+        // cancel the default action of each link
+        evt.preventDefault();
 
-            // cancel the default action of each link
-
+    })
 
     // move the focus to the first link
+    $("li:first-child a").focus();
+
+
+
 
 }); // end ready
